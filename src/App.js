e519f647d794d5ad73c57ef74126f7a69bc2948b
hsiangenlinlin <125904538+hsiangenlinlin@users.noreply.github.com>
2024-08-10 02:30:48 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import sketch from './sketch';
 import p5 from 'p5';
 
-const TEXT = 'Happy Birthday Olivia Akai Hsieh';
+const TEXT = 'Happy Birthday OliviaAkaiHsieh';
 
 const App = () => {
   const canvasRef = React.useRef(null)
