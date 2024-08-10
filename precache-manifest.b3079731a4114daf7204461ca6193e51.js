4b6039eb698e426288d84fef7127f491fb26121f
hsiang <shinelin89@gmail.com>
2024-08-10 19:21:26 +0100

    Updates

---
+++ precache-manifest.b3079731a4114daf7204461ca6193e51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f71734566386aba081b7a50093b2399e",
+    "url": "/happy-birthday-olivia-akai/index.html"
+  },
+  {
+    "revision": "cb007d9f51ce150a4402",
+    "url": "/happy-birthday-olivia-akai/static/css/main.e811170c.chunk.css"
+  },
+  {
+    "revision": "2a8ce47eb7bfc95b1c0d",
+    "url": "/happy-birthday-olivia-akai/static/js/2.78186614.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/happy-birthday-olivia-akai/static/js/2.78186614.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb007d9f51ce150a4402",
+    "url": "/happy-birthday-olivia-akai/static/js/main.0c971f84.chunk.js"
+  },
+  {
+    "revision": "db6dd5feec50837afb7a",
+    "url": "/happy-birthday-olivia-akai/static/js/runtime-main.57cddddf.js"
+  },
+  {
+    "revision": "de28f71ec6eb8dfda2e68d2211ee49eb",
+    "url": "/happy-birthday-olivia-akai/static/media/AvenirNextLTPro-Demi.de28f71e.otf"
+  }
+]);
